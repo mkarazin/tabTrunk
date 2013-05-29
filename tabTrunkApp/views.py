8387aea266aa529f5534e9b5ca59c9425cf6ea42
mkarazin <karazin@gmail.com>
2013-05-28 20:03:00 -0500

    changed indentation style

--- tabTrunkApp/views.py
+++ tabTrunkApp/views.py
@@ -10,69 +10,68 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-	tab_list = Tab.objects.filter(username=request.user.username).order_by('-addedDate')
-	#if tab_list.count() = 0
-	return render(request, 'tabTrunkApp/index.html',
-			{'tab_list' : tab_list},
-			context_instance=RequestContext(request),)
-				
+    tab_list = Tab.objects.filter(username=request.user.username).order_by('-addedDate')
+    #if tab_list.count() = 0
+    return render(request, 'tabTrunkApp/index.html',
+                  {'tab_list': tab_list},
+                  context_instance=RequestContext(request),)
 @login_required
 def detail(request, tab_id):
-	tab = get_object_or_404(Tab, pk=tab_id)
-	return render(request, 'tabTrunkApp/view.html', {'tab': tab},
-				context_instance=RequestContext(request),)
+    tab = get_object_or_404(Tab, pk=tab_id)
+    return render(request, 'tabTrunkApp/view.html', {'tab': tab},
+                  context_instance=RequestContext(request),)
 
 @login_required
 def edit(request, tab_id):
-	tab = get_object_or_404(Tab, pk=tab_id)
-	tabAbility = tab.get_ability_display()
-	
-	if (request.POST):
-		if(request.POST['submit'] == 'save'):
-			tab.songTitle = request.POST['songTitle']
-			tab.artist=request.POST['artist']
-	 		tab.tabURL=request.POST['tabURL']
-	 		tab.content=request.POST['content']
-	 		tab.save()
-	 	return render(request, 'tabTrunkApp/view.html', {'tab':tab},
-				context_instance=RequestContext(request),)
-	
-	return render(request, 'tabTrunkApp/edit.html', {'tab': tab, 'tabAbility' : tabAbility},
-				context_instance=RequestContext(request),)
+    tab = get_object_or_404(Tab, pk=tab_id)
+    tabAbility = tab.get_ability_display()
+
+    if request.POST:
+        if request.POST['submit'] == 'save':
+            tab.songTitle = request.POST['songTitle']
+            tab.artist=request.POST['artist']
+            tab.tabURL=request.POST['tabURL']
+            tab.content=request.POST['content']
+            tab.save()
+        return render(request, 'tabTrunkApp/view.html', {'tab':tab},
+                      context_instance=RequestContext(request),)
+
+    return render(request, 'tabTrunkApp/edit.html', {'tab': tab, 'tabAbility': tabAbility},
+                  context_instance=RequestContext(request),)
 
 @login_required
 def new(request):
-	return render(request, 'tabTrunkApp/new.html',
-	 			{'abilityChoices' : getattr(Tab, "abilityChoices") },
-	 			context_instance=RequestContext(request),)
+    return render(request, 'tabTrunkApp/new.html',
+                  {'abilityChoices': getattr(Tab, "abilityChoices") },
+                  context_instance=RequestContext(request),)
 @login_required
 def create(request):
-	
-	if(request.POST):
-		if(request.POST['submit'] == 'create'):
-			tab = Tab(
-				songTitle=request.POST['songTitle'],
-	 			artist=request.POST['artist'],
-	 			tabURL=request.POST['tabURL'],
-	 			content=request.POST['content'],
-	 			ability=request.POST['ability'],
-	 			addedDate=timezone.now(),
-				username=request.user.username)
 
-			tab.save()
-			return render(request, 'tabTrunkApp/view.html', {'tab': tab},
-				context_instance=RequestContext(request),)
-	return index(request)
+    if request.POST:
+        if request.POST['submit'] == 'create':
+            tab = Tab(
+                songTitle=request.POST['songTitle'],
+                artist=request.POST['artist'],
+                tabURL=request.POST['tabURL'],
+                content=request.POST['content'],
+                ability=request.POST['ability'],
+                addedDate=timezone.now(),
+                username=request.user.username)
+
+            tab.save()
+            return render(request, 'tabTrunkApp/view.html', {'tab': tab},
+                          context_instance=RequestContext(request),)
+    return index(request)
 
 @login_required
 def delete(request, tab_id):
-	tab = get_object_or_404(Tab, pk=tab_id)
-	
-	if(request.POST):
-		if (request.POST['submit'] == 'delete'):
-			tab.delete()
-			return index(request)
-		return detail(request, tab_id)
-	
-	return render(request, 'tabTrunkApp/confirmDelete.html', {'tab': tab},
-				context_instance=RequestContext(request),)
+    tab = get_object_or_404(Tab, pk=tab_id)
+
+    if request.POST:
+        if request.POST['submit'] == 'delete':
+            tab.delete()
+            return index(request)
+        return detail(request, tab_id)
+
+    return render(request, 'tabTrunkApp/confirmDelete.html', {'tab': tab},
+                  context_instance=RequestContext(request),)
